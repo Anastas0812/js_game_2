1a3b01584189c43347da88f5e491aa78e30f1c6f
anastasy_klimanova <klimanova_anastasy@mail.ru>
2025-09-08 00:24:27 +0300

    init

--- src/index.js
+++ src/index.js
@@ -36,6 +36,22 @@ export class Character {
     }
     this._type = value;
   }
+  levelUp() {
+    if (this.health <= 0) {
+      throw new Error('Персонаж мёртв. Повысить level невозможно'); 
+    }
+    this.health = 100;
+    this.level += 1;
+    this.attack += this.attack * 0.2;
+    this.defence += this.defence * 0.2;
+    return 'levelUp done';
+  }
+  damage(points) {
+    if (this.health >= 0) {
+      this.health -= points * (1 - this.defence / 100)
+    }
+    return `oops, ${this.name}, получил урон -${points}!`
+  }
 }
 
 export class Bowman extends Character {
@@ -93,6 +109,13 @@ const Dambldor = new Magician('dambldor', 'Magician', 700, 7, 20, 50);
 const CoulTurner = new Daemon('Coul', 'Daemon');
 const Azog = new Undead('Azog', 'Undead');
 const Shon = new Zombie('Shon', 'Zombie');
+
+// console.log(Deadshot.levelUp());
+console.log(Deadshot.damage(10));
+// console.log(Deadshot.level);
+// console.log(Deadshot.health);
+// console.log(Deadshot.attack);
+// console.log(Deadshot.defence);
 
 // void для линта, чтобы не ругался 
 
